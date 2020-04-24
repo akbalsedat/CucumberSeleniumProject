0b6d25abfe651f32b445409be68ceb192e5148d5
akbalsedat <akbalsedat@gmail.com>
2020-04-24 17:54:25 -0400

    Update CucumberRunner.java

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -5,29 +5,19 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-
 @CucumberOptions(
-<<<<<<< HEAD
-        features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
-        strict = true,
-        dryRun = false
+        features = "src/test/resources",
+        dryRun = false,
+        strict = false,
+        tags = "@create_car_ddt",
+        plugin = {
+                "html:target/default-report",
+                "json:target/cucumber1.json"
+        }
+
 )
 public class CucumberRunner {
 
-=======
-        glue = "com/vytrack/step_definitions", // set: The paths of the step definition files
-        features = "src/test/resources/features", // set: The paths of the feature files
-        plugin = {"pretty",
-                "html:target/cucumber-reports"
-        },
-        dryRun = false,
-        strict = true,
-        monochrome = true, // Display the console Output in much readable way
-       // tags = "@driver" // to run scenarios that have this tags
-       // tags = "not @driver" // run every other scenarios except this
-        tags = "@login_with_params"
-)
-public class CucumberRunner {
->>>>>>> New Calendar Future File
+
 }
