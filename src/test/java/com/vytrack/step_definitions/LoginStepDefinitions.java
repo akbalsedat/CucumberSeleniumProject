1e7a36575dba3a691f4c2b879e9187ba8b51b52b
akbalsedat <akbalsedat@gmail.com>
2020-04-24 18:09:22 -0400

    Update LoginStepDefinitions.java

--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -1,34 +1,24 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.pages.LoginPage;
+import com.vytrack.utilities.BrowserUtilities;
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class LoginStepDefinitions {
-    @Given("user is on the landing page")
-    public void user_is_on_the_landing_page() {
-        System.out.println("I am on the login page.");
+    LoginPage loginPage = new LoginPage();
 
-    }
-
-    @Then("user logs in as store manager")
-    public void user_logs_in_as_store_manager() {
-        System.out.println("Login as store manager");
-    }
-    @Then("User verifies that {string} page subtitle is displayed")
-    public void user_verifies_that_page_subtitle_is_displayed(String string) {
-        System.out.println("Verifying page subtitle: " + string);
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+        System.out.println("Open login page");
+        String URL = ConfigurationReader.getProperty("qa1");
+        Driver.getDriver().get(URL);
     }
 
-    @Then("user logs in as driver")
-    public void user_logs_in_as_driver() {
-        System.out.println("User logs in as driver");
-    }
-
-    @Then("user logs in as sale manager")
-    public void user_logs_in_as_sale_manager() {
-        System.out.println("User logs in as sale manager");
-    }
-    
     @When("user logs in as a sales manager")
     public void user_logs_in_as_a_sales_manager() {
         System.out.println("Login as sales manager");
@@ -37,8 +27,16 @@ public class LoginStepDefinitions {
 
     @When("user logs in as a store manager")
     public void user_logs_in_as_a_store_manager() {
-        System.out.println("Login as store manager");
         loginPage.login("storemanager85", "UserUser123");
+    }
+
+
+    @Then("user should verify that title is a Dashboard")
+    public void user_should_verify_that_title_is_a_Dashboard() {
+        System.out.println("Verify that title is a Dashboard");
+        BrowserUtilities.waitForPageToLoad(10);
+        BrowserUtilities.wait(2);
+        Assert.assertEquals("Dashboard", Driver.getDriver().getTitle());
     }
 
     @When("user logs in as a driver")
@@ -47,16 +45,34 @@ public class LoginStepDefinitions {
         loginPage.login("user19", "UserUser123");
     }
 
-    @Then("user should verify that title is a Dashboard")
-    public void user_should_verify_that_title_is_a_Dashboard() {
-        System.out.println("Verify that title is a Dashboard");
-        Assert.assertEquals("Dashboard", Driver.getDriver().getTitle());
+    //    When user enters "storemanager85" username and "UserUser123" password
+    @When("user enters {string} username and {string} password")
+    public void user_enters_username_and_password(String string, String string2) {
+        System.out.printf("Login with user name %s and %s password\n", string, string2);
+        loginPage.login(string, string2);
     }
 
-    @When("User enters {string} username and {string} password")
-    public void user_enters_username_and_password(String string, String string2) {
-        System.out.printf("Login with username %s and %s password\n", string, string2);
-        loginPage.login(string, string2);
-        Assert.assertEquals("Dashboard", Driver.getDriver().getTitle());
+    @When("user navigates to {string} and {string}")
+    public void user_navigates_to_and(String tab, String module) {
+        System.out.printf("User clicks on the %s tab and navigates to %s module\n", tab, module);
+        loginPage.navigateTo(tab, module);
     }
+
+    @Then("user name should be {string}")
+    public void user_name_should_be(String string) {
+        Assert.assertEquals(string, loginPage.getCurrentUserName());
+    }
+
+
+    @When("user logs in as {string}")
+    public void user_logs_in_as(String userType) {
+        loginPage.login(userType);
+    }
+
+    @Then("user verifies that page title is {string}")
+    public void user_verifies_that_page_title_is(String string) {
+        System.out.println("Verify that page title is: " + string);
+        Assert.assertEquals(string, Driver.getDriver().getTitle());
+    }
+
 }
